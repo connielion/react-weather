dc274514f17be1d332e931594d4441b65a891fe7
Connie <clai011@ucr.edu>
2019-06-19 19:26:10 -0700

    api call not working

--- src/index.js
+++ src/index.js
@@ -6,7 +6,6 @@ import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import * as serviceWorker from './serviceWorker';
 require('dotenv').config();
-
 ReactDOM.render(<App />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
