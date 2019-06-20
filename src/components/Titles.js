4aa9be878cdcab43630e83346dd3e2c8a1d27d4f
Connie <clai011@ucr.edu>
2019-06-20 13:45:47 -0700

    font color change

--- src/components/Titles.js
+++ src/components/Titles.js
@@ -2,16 +2,14 @@ import React from 'react';
 
 const Titles = () => (
 	<div>
-		<h1 className="title-container__title"> React.js Weather </h1>
-		<p className="title-container__subtitle"> Find out current temperature and conditions... </p>
+		<h1 className="title-container__title"> Wreather </h1>
+		<p className="title-container__subtitle"> Find temperatures, conditions, and more... </p>
 		<button className="btn btn-default title-container__subtitle">
-			<a href="https://github.com/connielion/react-weather">Link to project Github Repository</a>
+			<a href="https://github.com/connielion/react-weather" className="gh-link">Link to project Github Repository</a>
 		</button>
 		<p className="title-container__subtitle">Made with React.js,OpenWeatherMap API, Bootstrap & Google Fonts.</p>
 		<hr />
-		<button className=" btn btn-default title-container__subtitle">
-			<a href="https:www.connielion.com" target="_blank">My website</a>
-		</button>
+
 	</div>
 );
 export default Titles;
