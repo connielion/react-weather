a79313bc9182712db0bd619d0d1cb1f0c6a565e2
Connie <clai011@ucr.edu>
2019-06-19 20:56:51 -0700

    Updates

---
+++ precache-manifest.2954a88867115bfa43368499301ffc16.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c7eca7c70e8ceeda9ce0ba013e92b3c3",
+    "url": "/react-weather/index.html"
+  },
+  {
+    "revision": "b4c9a642149064c4cb66",
+    "url": "/react-weather/static/css/2.c5174c75.chunk.css"
+  },
+  {
+    "revision": "9fddcff6fcc2981f9230",
+    "url": "/react-weather/static/css/main.6a5b0621.chunk.css"
+  },
+  {
+    "revision": "b4c9a642149064c4cb66",
+    "url": "/react-weather/static/js/2.3839f2b0.chunk.js"
+  },
+  {
+    "revision": "9fddcff6fcc2981f9230",
+    "url": "/react-weather/static/js/main.132aef97.chunk.js"
+  },
+  {
+    "revision": "e61e452087af7072f6e9",
+    "url": "/react-weather/static/js/runtime~main.2302d1fd.js"
+  },
+  {
+    "revision": "577ccb7979040dbd2b152ea9a6137636",
+    "url": "/react-weather/static/media/bg.577ccb79.jpg"
+  },
+  {
+    "revision": "448c34a56d699c29117adc64c43affeb",
+    "url": "/react-weather/static/media/glyphicons-halflings-regular.448c34a5.woff2"
+  },
+  {
+    "revision": "89889688147bd7575d6327160d64e760",
+    "url": "/react-weather/static/media/glyphicons-halflings-regular.89889688.svg"
+  },
+  {
+    "revision": "e18bbf611f2a2e43afc071aa2f4e1512",
+    "url": "/react-weather/static/media/glyphicons-halflings-regular.e18bbf61.ttf"
+  },
+  {
+    "revision": "f4769f9bdb7466be65088239c12046d1",
+    "url": "/react-weather/static/media/glyphicons-halflings-regular.f4769f9b.eot"
+  },
+  {
+    "revision": "fa2772327f55d8198301fdb8bcfc8158",
+    "url": "/react-weather/static/media/glyphicons-halflings-regular.fa277232.woff"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.d4b442fbee6f5f52d84bc709ad8274f7.js"
+  "/react-weather/precache-manifest.2954a88867115bfa43368499301ffc16.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/react-weather/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
