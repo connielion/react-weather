915f43256799d2d12366186d1c3ed909d7f54fac
Connie <clai011@ucr.edu>
2019-06-19 20:34:33 -0700

    Updates

---
+++ precache-manifest.91343fe9612465de7fd13fc42ca39fab.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "364ea9d022df09674dd7403376613d3a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5d5679a2e55d8a78ff3f",
+    "url": "/static/css/2.9357dba0.chunk.css"
+  },
+  {
+    "revision": "2c6da86e611daf967149",
+    "url": "/static/css/main.d958d7f6.chunk.css"
+  },
+  {
+    "revision": "5d5679a2e55d8a78ff3f",
+    "url": "/static/js/2.cf71b94b.chunk.js"
+  },
+  {
+    "revision": "2c6da86e611daf967149",
+    "url": "/static/js/main.81a63835.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "577ccb7979040dbd2b152ea9a6137636",
+    "url": "/static/media/bg.577ccb79.jpg"
+  },
+  {
+    "revision": "448c34a56d699c29117adc64c43affeb",
+    "url": "/static/media/glyphicons-halflings-regular.448c34a5.woff2"
+  },
+  {
+    "revision": "89889688147bd7575d6327160d64e760",
+    "url": "/static/media/glyphicons-halflings-regular.89889688.svg"
+  },
+  {
+    "revision": "e18bbf611f2a2e43afc071aa2f4e1512",
+    "url": "/static/media/glyphicons-halflings-regular.e18bbf61.ttf"
+  },
+  {
+    "revision": "f4769f9bdb7466be65088239c12046d1",
+    "url": "/static/media/glyphicons-halflings-regular.f4769f9b.eot"
+  },
+  {
+    "revision": "fa2772327f55d8198301fdb8bcfc8158",
+    "url": "/static/media/glyphicons-halflings-regular.fa277232.woff"
+  }
+]);
